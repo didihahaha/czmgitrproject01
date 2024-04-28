5785b37163e3828c1dda0e4f5105a5d618957d8f
czm <zimingmorning520@gmail.com>
2024-04-28 17:16:46 -0400

    commit 01: Hi.class

---
+++ crm/src/com/czm/Hi.java
@@ -0,0 +1,11 @@
+package com.czm;
+
+/**
+ * @author Ziming Chen
+ * @version 1.0
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("hi this is my first project associated with Git");
+    }
+}
